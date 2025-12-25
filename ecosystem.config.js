9152e0d5c905abc97b5c571742c1c3eaf78febcb
yubinghua <981842762@qq.com>
2025-12-25 18:23:23 +0800

    feat: udpate

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,8 +8,8 @@ module.exports = {
     env: {
       NODE_ENV: 'production',
       PORT: 3000,
-      NEXT_PUBLIC_BACKEND_URL: 'http://149.88.88.205:4001',
-      NEXT_PUBLIC_SOCKET_URL: 'http://149.88.88.205:4001'
+      NEXT_PUBLIC_BACKEND_URL: 'https://rubywu.top',
+      NEXT_PUBLIC_SOCKET_URL: 'https://rubywu.top'
     },
     error_file: './logs/pm2-error.log',
     out_file: './logs/pm2-out.log',
